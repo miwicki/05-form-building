7838a9a364c1c44a18cb8888d5f25fa19291d43e
Matt <wrestler.411@comcast.net>
2017-11-08 19:35:16 -0800

    finishing this lab finally

--- matt-iwicki/scripts/articleView.js
+++ matt-iwicki/scripts/articleView.js
@@ -1,140 +1,139 @@
 'use strict';
- +
- +let articleView = {};
- +
- +articleView.populateFilters = () => {
- +  $('article').each(function() {
- +    if (!$(this).hasClass('template')) {
- +      let val = $(this).find('address a').text();
- +      let optionTag = `<option value="${val}">${val}</option>`;
- +
- +      if ($(`#author-filter option[value="${val}"]`).length === 0) {
- +        $('#author-filter').append(optionTag);
- +      }
- +
- +      val = $(this).attr('data-category');
- +      optionTag = `<option value="${val}">${val}</option>`;
- +      if ($(`#category-filter option[value="${val}"]`).length === 0) {
- +        $('#category-filter').append(optionTag);
- +      }
- +    }
- +  });
- +};
- +
- +articleView.handleAuthorFilter = () => {
- +  $('#author-filter').on('change', function() {
- +    if ($(this).val()) {
- +      $('article').hide();
- +      $(`article[data-author="${$(this).val()}"]`).fadeIn();
- +    } else {
- +      $('article').fadeIn();
- +      $('article.template').hide();
- +    }
- +    $('#category-filter').val('');
- +  });
- +};
- +
- +articleView.handleCategoryFilter = () => {
- +  $('#category-filter').on('change', function() {
- +    if ($(this).val()) {
- +      $('article').hide();
- +      $(`article[data-category="${$(this).val()}"]`).fadeIn();
- +    } else {
- +      $('article').fadeIn();
- +      $('article.template').hide();
- +    }
- +    $('#author-filter').val('');
- +  });
- +};
- +
- +articleView.handleMainNav = () => {
- +  $('.main-nav').on('click', '.tab', function() {
- +    $('.tab-content').hide();
- +    $('#' + $(this).data('content')).fadeIn();
- +  });
- +
- +  $('.main-nav .tab:first').click();
- +};
- +
- +articleView.setTeasers = () => {
- +  $('.article-body *:nth-of-type(n+2)').hide();
- +  $('article').on('click', 'a.read-on', function(e) {
- +    e.preventDefault();
- +    if ($(this).text() === 'Read on →') {
- +      $(this).parent().find('*').fadeIn();
- +      $(this).html('Show Less &larr;');
- +    } else {
- +      $('body').animate({
- +        scrollTop: ($(this).parent().offset().top)
- +      },200);
- +      $(this).html('Read on &rarr;');
- +      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
- +    }
- +  });
- +};
- +
- +// COMMENT: Where is this function called? Why?
- +// This function is called on the bottom of the new.html in a <script> tag.
- +// it's called at the bottom of the page instead of in the javascript so we can use the same javascript for different pages.
- +articleView.initNewArticlePage = () => {
- +  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
- +  $('.tab-content').fadeIn();
- +
- +  // DONE: The new articles we create will be copy/pasted into our source data file.
- +  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
- +  $('#article-export').hide();
- +  $('#copyResponse').hide();
- +
- +  $('#article-json').on('focus', function(){
- +    this.select();
- +    document.execCommand('copy');
- +    $('#copyResponse').show();
- +    $('#copyResponse').fadeOut(3000);
- +  });
- +
- +  // DONE: Add an event handler to update the preview and the export field if any inputs change.
- +  $('#new-form').on('change keydown keyup', 'input, textarea', articleView.create);
- +};
- +
- +articleView.create = () => {
- +  // DONE: Set up a variable to hold the new article we are creating.
- +  // Clear out the #articles element, so we can put in the updated preview
- +  let newArticle;
- +  $('#articles').empty();
- +
- +  // DONE: Instantiate an article based on what's in the form fields:
- +  newArticle = new Article({
- +    title: $('#article-title').val(),
- +    category: $('#article-category').val(),
- +    author: $('#article-author').val(),
- +    authorUrl: $('#article-author-url').val(),
- +    body: $('#article-body').val().replace(/^`{3}([^]*?)`{3}$/gm, '<pre><code class="html">$1</code></pre>'),
- +    publishedOn: $('#article-published:checked').length
- +  })
- +  // console.log(newArticle.body.replace(/^`{3}([^]*?)`{3}$/gm, '<pre><code> $1' + '</pre></code>')); // <-- trying to replace  _```_ with _<pre><code>_ but can't seem to figure out how to get the ending </pre></code>
- +
- +  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
- +  let articleTemplate = Handlebars.compile($('#article-template').html());
- +  $('#articles').append(articleTemplate(newArticle));
- +
- +
- +  // DONE: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
- +  $('pre code').each(function(i, block) {
- +    hljs.highlightBlock(block);
- +  });
- +
- +  // DONE: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
- +  $('#article-export').show();
- +  $('#article-json').val(JSON.stringify(newArticle));
- +};
- +
- +// COMMENT: Where is this function called? Why?
- +// This function is called in index.html to load all of the filter titles, and load all of the articles.
- +articleView.initIndexPage = () => {
- +  articles.forEach(article => $('#articles').append(article.toHtml())); // eslint-disable-line
- +  articleView.populateFilters();
- +  articleView.handleCategoryFilter();
- +  articleView.handleAuthorFilter();
- +  articleView.handleMainNav();
- +  articleView.setTeasers();
- +};
+
+let articleView = {};
+
+articleView.populateFilters = () => {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      let val = $(this).find('address a').text();
+      let optionTag = `<option value="${val}">${val}</option>`;
+
+      if ($(`#author-filter option[value="${val}"]`).length === 0) {
+        $('#author-filter').append(optionTag);
+      }
+
+      val = $(this).attr('data-category');
+      optionTag = `<option value="${val}">${val}</option>`;
+      if ($(`#category-filter option[value="${val}"]`).length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+articleView.handleAuthorFilter = () => {
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-author="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+articleView.handleCategoryFilter = () => {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $(`article[data-category="${$(this).val()}"]`).fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+articleView.handleMainNav = () => {
+  $('.main-nav').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+
+  $('.main-nav .tab:first').click();
+};
+
+articleView.setTeasers = () => {
+  $('.article-body *:nth-of-type(n+2)').hide();
+  $('article').on('click', 'a.read-on', function(e) {
+    e.preventDefault();
+    if ($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      },200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
+  });
+};
+
+// COMMENT: Where is this function called? Why?
+// This function is called on the bottom of the new.html in a <script> tag.
+// it's called at the bottom of the page instead of in the javascript so we can use the same javascript for different pages.
+articleView.initNewArticlePage = () => {
+  // DONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  $('.tab-content').fadeIn();
+
+  // DONE: The new articles we create will be copy/pasted into our source data file.
+  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+  $('#article-export').hide();
+  $('#copyResponse').hide();
+
+  $('#article-json').on('focus', function(){
+    this.select();
+    document.execCommand('copy');
+    $('#copyResponse').show();
+    $('#copyResponse').fadeOut(3000);
+  });
+
+  // DONE: Add an event handler to update the preview and the export field if any inputs change.
+  $('#new-form').on('change keydown keyup', 'input, textarea', articleView.create);
+};
+
+articleView.create = () => {
+  // DONE: Set up a variable to hold the new article we are creating.
+  // Clear out the #articles element, so we can put in the updated preview
+  let newArticle;
+  $('#articles').empty();
+
+  // DONE: Instantiate an article based on what's in the form fields:
+  newArticle = new Article({
+    title: $('#article-title').val(),
+    category: $('#article-category').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    body: $('#article-body').val().replace(/^`{3}\n([^]*?)`{3}$/gm, `<pre><code>$1</code></pre>`),
+    publishedOn: $('#article-published:checked').length
+  })
+
+  // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
+  let articleTemplate = Handlebars.compile($('#article-template').html());
+  $('#articles').append(articleTemplate(newArticle));
+
+
+  // DONE: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
+
+  // DONE: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#article-export').show();
+  $('#article-json').val(JSON.stringify(newArticle));
+};
+
+// COMMENT: Where is this function called? Why?
+// This function is called in index.html to load all of the filter titles, and load all of the articles.
+articleView.initIndexPage = () => {
+  articles.forEach(article => $('#articles').append(article.toHtml())); // eslint-disable-line
+  articleView.populateFilters();
+  articleView.handleCategoryFilter();
+  articleView.handleAuthorFilter();
+  articleView.handleMainNav();
+  articleView.setTeasers();
+};
--- matt-iwicki/scripts/blogArticles.js
+++ matt-iwicki/scripts/blogArticles.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var rawData = [
-  {"author":"JB Tellez","authorUrl":"JBFTW.party","title":"Let's write an article together","category":"codez","body":"<p>I make codez. So do you.</p>\n<pre><code>function Article (rawDataObj) {\n  this.author = rawDataObj.author;\n  this.authorUrl = rawDataObj.authorUrl;\n  this.title = rawDataObj.title;\n  this.category = rawDataObj.category;\n  this.body = rawDataObj.body;\n  this.publishedOn = rawDataObj.publishedOn;\n}\n</code></pre>","publishedOn":"2017-10-27T22:57:02.858Z","daysAgo":0,"publishStatus":"published 0 days ago"},
+var rawData = [ // eslint-disable-line
   {
     title:       'Bacon Ipsum',
     category:    'food',
